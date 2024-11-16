21f0c6e9a47386e5ba23d3c6c3abda927c744f10
Aniket Jivani <ajivani@umich.edu>
2024-11-15 23:24:18 -0500

    preliminary working version of kfold selection

--- 1d_toy/botorch_optim_point_selection_1d_final.py
+++ 1d_toy/botorch_optim_point_selection_1d_final.py
@@ -1,31 +1,7 @@
-import numpy as np
-import torch
-import gpytorch
-import botorch
-import os
 import sys
-import pickle
-from botorch.models import SingleTaskGP, FixedNoiseGP, ModelListGP
-from botorch.models.transforms import Normalize, Standardize
-from botorch.acquisition.analytic import LogExpectedImprovement
-from botorch.fit import fit_gpytorch_model
-from botorch.optim import optimize_acqf
-import pickle
-
-import matplotlib.pyplot as plt
-
-plt.rcParams['figure.dpi'] = 200
-plt.style.use('seaborn-v0_8-notebook')
-plt.rc("font", family="serif")
-plt.rc("axes.spines", top=True, right=True)
-# set explicit fontsizes for ticks, lables, legend and title
-plt.rc('xtick', labelsize=14)
-plt.rc('ytick', labelsize=14)
-plt.rc('axes', labelsize=16)
-plt.rc('legend', fontsize=14)
-plt.rc('figure', titlesize=16)
-
 # %%
+
+# print(sys.argv)
 
 torch_seed = sys.argv[1]
 lb = [sys.argv[2], sys.argv[3]]
@@ -35,7 +11,6 @@ data_dir = sys.argv[7]
 input_dir = sys.argv[8]
 rep_num = sys.argv[9]
 batch_num = sys.argv[10]
-
 cv_errors = sys.argv[11]
 hf_inputs = sys.argv[12] 
 lf_inputs = sys.argv[13]
@@ -53,8 +28,36 @@ print("input_dir: ", input_dir)
 print("rep_num: ", rep_num)
 print("batch_num: ", batch_num)
 
-torch.manual_seed(torch_seed)
-os.makedirs(os.path.join(plot_dir, "rep_{:03d}".format(rep_num), "batch_{:02d}".format(batch_num)), exist_ok=True)
+# %%
+import numpy as np
+import torch
+import gpytorch
+import botorch
+import os
+import sys
+import pickle
+from botorch.models import SingleTaskGP, FixedNoiseGP, ModelListGP
+from botorch.models.transforms import Normalize, Standardize
+from botorch.acquisition.analytic import LogExpectedImprovement, ExpectedImprovement
+from botorch.fit import fit_gpytorch_model
+from botorch.optim import optimize_acqf
+
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+
+plt.rcParams['figure.dpi'] = 200
+plt.style.use('seaborn-v0_8-notebook')
+plt.rc("font", family="serif")
+plt.rc("axes.spines", top=True, right=True)
+# set explicit fontsizes for ticks, lables, legend and title
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+plt.rc('axes', labelsize=16)
+plt.rc('legend', fontsize=14)
+plt.rc('figure', titlesize=16)
+
+
+# torch.manual_seed(torch_seed)
 
 # %%
 random_acq = torch.rand(2)
@@ -67,7 +70,9 @@ random_acq_rescaled = torch.unsqueeze(random_acq * (torch.tensor(ub) - torch.ten
 
 # %%
 
-train_X = torch.tensor(hf_inputs, dtype=torch.double)
+nHF = int(hf_inputs.shape[0] / 2)
+
+train_X = torch.tensor(hf_inputs[:nHF, :], dtype=torch.double)
 train_Y = torch.tensor(cv_errors, dtype=torch.double).unsqueeze(-1)
 
 gp = SingleTaskGP(
@@ -78,51 +83,57 @@ gp = SingleTaskGP(
 
 mll = gpytorch.mlls.ExactMarginalLogLikelihood(gp.likelihood, gp)
 fit_gpytorch_model(mll)
-logEI = LogExpectedImprovement(model=gp, best_f=train_Y.max())
+# acq = LogExpectedImprovement(model=gp, best_f=train_Y.max())
+acq = ExpectedImprovement(model=gp, best_f=train_Y.max())
+
 
 bounds = torch.stack([torch.tensor(lb), 
                       torch.tensor(ub)]).to(torch.double)
+
+print("Running Acquisition.")
 candidate, acq_value = optimize_acqf(
-    logEI, 
+    # logEI, 
+    acq,
     bounds=bounds, 
     q=1, 
     num_restarts=5, 
     raw_samples=20,
 )
 
+print("Acquisition complete.")
 # %% Save GP object to file:
-with open(os.path.join(data_dir, "rep_{:03d}".format(rep_num), "batch_{:02d}_gp.pkl".format(batch_num),
-                       "wb")) as f:
-    pickle.dump({"gp": gp, 
-                 "utility": logEI}, 
-                 f, 
-                 protocol=pickle.HIGHEST_PROTOCOL)
+# with open(os.path.join(data_dir, "rep_{:03d}".format(rep_num), "batch_{:02d}_gp.pkl".format(batch_num),
+#                        "wb")) as f:
+#     pickle.dump({"gp": gp, 
+#                  "utility": logEI}, 
+#                  f, 
+#                  protocol=pickle.HIGHEST_PROTOCOL)
 
 print("Random acquisition: ", random_acq_rescaled.T)
 print("Active learnt acquisition: ", candidate)
 
 # %%
-nLF = lf_inputs.shape[0]
-nHF = hf_inputs.shape[0]
+nLF = int(lf_inputs.shape[0]/2)
+# nHF = hf_inputs.shape[0]
 
 augmented_LF = np.zeros((2 * (nLF + 1), 2))
-augmented_LF[:nLF, :] = lf_inputs
+augmented_LF[:nLF, :] = lf_inputs[:nLF, :]
 augmented_LF[nLF:(nLF + 1), :] = candidate.detach().numpy()
 
-augmented_LF[(nLF + 1):(2 * (nLF + 1) - 1), :] = lf_inputs
+augmented_LF[(nLF + 1):(2 * (nLF + 1) - 1), :] = lf_inputs[nLF:, :]
 augmented_LF[(2 * (nLF + 1) - 1), :] = random_acq_rescaled.T.detach().numpy()
 
 augmented_HF = np.zeros((2 * (nHF + 1), 2))
-augmented_HF[:nHF, :] = hf_inputs
+augmented_HF[:nHF, :] = hf_inputs[:nHF, :]
 augmented_HF[nHF:(nHF + 1), :] = candidate.detach().numpy()
-augmented_HF[(nHF + 1):(2 * (nHF + 1) - 1), :] = hf_inputs
+augmented_HF[(nHF + 1):(2 * (nHF + 1) - 1), :] = hf_inputs[nHF:, :]
 augmented_HF[(2 * (nHF + 1) - 1), :] = random_acq_rescaled.T.detach().numpy()
 
-augmented_LF_subset = np.zeros((2 * (nHF + 1), 2))
-augmented_LF_subset[:nHF, :] = lf_subset
-augmented_LF_subset[nHF:(nHF + 1), :] = nLF + 1
-augmented_LF_subset[(nHF + 1):(2 * (nHF + 1) - 1), :] = lf_subset
-augmented_LF_subset[(2 * (nHF + 1) - 1), :] = nLF + 1
+augmented_LF_subset = np.zeros((2 * (nHF + 1)))
+augmented_LF_subset[:nHF] = lf_subset[:nHF]
+augmented_LF_subset[nHF:(nHF + 1)] = nLF + 1
+augmented_LF_subset[(nHF + 1):(2 * (nHF + 1) - 1)] = lf_subset[nHF:]
+augmented_LF_subset[(2 * (nHF + 1) - 1)] = nLF + 1
 
 
 # save to input dir with appropriate rep and batch numbering.
@@ -136,32 +147,8 @@ np.savetxt(os.path.join(input_dir,
 
 np.savetxt(os.path.join(input_dir,
                         "rep_{:03d}".format(rep_num),
-                        "HF_Batch_{:03d}_Subset_Final.txt".format(batch_num + 1)), HFSubsetAugmented, fmt="%d")
+                        "HF_Batch_{:03d}_Subset_Final.txt".format(batch_num + 1)), augmented_LF_subset, fmt="%d")
 
-
-# %%
-# augment the training data with the new candidate values and save to respective text files.
-
-
-# augmented_LF = np.zeros((mLF + 1, nLF))
-#     augmented_LF[:mLF, :] = xiLF
-#     augmented_LF[mLF, :] = tgEI[0]
-
-#     HFSubsetAugmented = np.append(HFSubset, [mLF + 1])
-
-#     # Save inputs to text files and acquired GP to pickle file. Use the newly generated text files to acquire fresh HF and LF simulation.
-
-#     np.savetxt(os.path.join(args.dirInputs,
-#                             "rep_{:03d}".format(args.repNum),
-#                             "HF_Batch_{:03d}_Final.txt".format(batch_num + 1)), augmented_HF)
-
-#     np.savetxt(os.path.join(args.dirInputs,
-#                             "rep_{:03d}".format(args.repNum),
-#                             "LF_Batch_{:03d}_Final.txt".format(batch_num + 1)), augmented_LF)
-
-#     np.savetxt(os.path.join(args.dirInputs,
-#                             "rep_{:03d}".format(args.repNum),
-#                             "HF_Batch_{:03d}_Subset_Final.txt".format(batch_num + 1)), HFSubsetAugmented, fmt="%d")
 #%% Test points for contour plots.
 
 ntest = 30
@@ -181,22 +168,24 @@ with torch.no_grad():
     variance_gp = posterior_unscaled.variance
 
     # lower_gp, upper_gp = posterior_dist.mvn.confidence_region()
-    utilities = logEI(test_X.unsqueeze(dim=1))
+    # utilities = logEI(test_X.unsqueeze(dim=1))
+    utilities = acq(test_X.unsqueeze(dim=1))
+
 
 fig, ax = plt.subplots(1, 3, figsize=(11, 4))
 
 # plot the predictive mean
 ct1 = ax[0].contourf(test_X1_grid, test_X2_grid, np.exp(mean_gp.reshape(ntest, ntest)), levels=25, cmap='viridis')
 ax[0].set_title('Mean')
-fig.colorbar(ct1, ax=ax[0], fraction=0.046, pad=0.04)
+c1 = fig.colorbar(ct1, ax=ax[0], fraction=0.046, pad=0.04)
 # plot the predictive variance
 ct2 = ax[1].contourf(test_X1_grid, test_X2_grid, torch.sqrt(variance_gp.reshape(ntest, ntest)), levels=25, cmap='viridis')
 ax[1].set_title('Std. Dev.')
-fig.colorbar(ct2, ax=ax[1], fraction=0.046, pad=0.04)
+c2 = fig.colorbar(ct2, ax=ax[1], fraction=0.046, pad=0.04)
 # plot the utility
 ct3 = ax[2].contourf(test_X1_grid, test_X2_grid, utilities.reshape(ntest, ntest), levels=25, cmap='viridis')
 ax[2].set_title('Utility')
-fig.colorbar(ct3, ax=ax[2], fraction=0.046, pad=0.04)
+c3 = fig.colorbar(ct3, ax=ax[2], fraction=0.046, pad=0.04)
 # Now overlay selected candidate on utility plot
 ax[2].scatter(candidate[0][0], candidate[0][1], 
             s=100,
@@ -206,6 +195,11 @@ ax[2].scatter(candidate[0][0], candidate[0][1],
             linewidths=1.5,
             clip_on=False,
 )
+
+c1.ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+c2.ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+c3.ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+
 # change aspect ratio of the plots
 for ia, a in enumerate(ax):
     a.set_aspect('equal')
@@ -218,7 +212,7 @@ plt.tight_layout()
 # save and remove margins
 plt.savefig(os.path.join(plot_dir, 
             "rep_{:03d}".format(rep_num), 
-            "batch_{:02d}".format(batch_num),"bo_contour_logEI_1d.png"), 
+            "batch_{:02d}_contours_EI_1d.png".format(batch_num)), 
             bbox_inches='tight')
 
 plt.close()
--- 1d_toy/kle_surrogate_building_1d_final.jl
+++ 1d_toy/kle_surrogate_building_1d_final.jl
@@ -24,7 +24,7 @@ include("./utils.jl")
 
 kle_kwargs_Δ = (useFullGrid=1,
             getAllModes=0,
-            order=2,
+            order=3,
             dims=2,
             weightFunction=getWeights,
             family="Legendre",
@@ -44,25 +44,14 @@ args_dict = Dict("plot_dir"=> "./Plots/1d_toy_plots",
             "data_dir"=> "./1d_toy/1d_pred_data",
             "input_dir"=> "./1d_toy/1d_inputs",
             "NREPS"=> 1,
-            "NFOLDS"=> 3,
+            "NFOLDS"=> 5,
             "BUDGET_HF"=>20,
             "lb" => [40, 30],
             "ub" => [60, 50],
        )
 
 sys = pyimport("sys")
-sys.argv = ["./botorch_optim_point_selection_1d_final.py",
-            args_dict["plot_dir"],
-            args_dict["data_dir"],
-            args_dict["input_dir"],
-            args_dict["NREPS"],
-            args_dict["NFOLDS"],
-            args_dict["BUDGET_HF"],
-            args_dict["lb"][1],
-            args_dict["lb"][2],
-            args_dict["ub"][1],
-            args_dict["ub"][2]
-            ]
+pkl = pyimport("pickle")
 
 kle_cases = ["gp", "ra"]
 
@@ -91,11 +80,9 @@ else
     HF_oracle = JLD.load("./1d_toy/HF_Oracle.jld")["HF_oracle"]
 end
 
-
-
 ## Loop for building surrogate
 
-
+# repID = 1
 for repID in 1:args_dict["NREPS"]
     println("Starting repetition $repID")
     # Specify a new random seed for each repetition
@@ -107,6 +94,7 @@ for repID in 1:args_dict["NREPS"]
     mkpath(joinpath(args_dict["input_dir"], @sprintf("rep_%03d", repID)))
     mkpath(joinpath(args_dict["data_dir"], @sprintf("rep_%03d", repID)))
 
+    # batchID = 0
     for batchID in 0:(args_dict["BUDGET_HF"] - 1)
         if batchID == 0
             isPilot = true
@@ -116,16 +104,16 @@ for repID in 1:args_dict["NREPS"]
         else
             isPilot = false
             fileLF = joinpath(args_dict["input_dir"], 
-            @sprintf("rep_%03d", rep_num), 
-            @sprintf("LF_Batch_%03d_Final", CURRENT_BT) * ".txt"
+            @sprintf("rep_%03d", repID), 
+            @sprintf("LF_Batch_%03d_Final", batchID) * ".txt"
             )
             fileHF = joinpath(args_dict["input_dir"], 
-            @sprintf("rep_%03d", rep_num),
-            @sprintf("HF_Batch_%03d_Final", CURRENT_BT) * ".txt"
+            @sprintf("rep_%03d", repID),
+            @sprintf("HF_Batch_%03d_Final", batchID) * ".txt"
             )
             fileHFIdx = joinpath(args_dict["input_dir"], 
-            @sprintf("rep_%03d", rep_num),
-            @sprintf("HF_Batch_%03d_Subset_Final", CURRENT_BT) * ".txt"
+            @sprintf("rep_%03d", repID),
+            @sprintf("HF_Batch_%03d_Subset_Final", batchID) * ".txt"
             )
         end
     
@@ -139,49 +127,123 @@ for repID in 1:args_dict["NREPS"]
         nLF = Int(size(inputsLF, 1) / 2)
         nHF = Int(size(inputsHF, 1) / 2)
 
-        batch_num = size(inputsLF, 1) - nPilotLF
+        nPilotLF = 200
+        nPilotHF = 5
 
-        @assert CURRENT_BT == batch_num
+        batch_num = Int(size(inputsLF, 1) / 2) - nPilotLF
 
-        println("Batch number: $batch_num", "LF Points: $nLF", "HF Points: $nHF")
+        @assert batchID == batch_num
+
+        println("Batch number: $batch_num", " LF Points:  $nLF", " HF Points: $nHF")
 
 
         # Rescale acquired points which are between (-1, 1) to original domain.
-        inputsLF_orig = 0.5 * inputsLF .* (ub - lb)' .+ 0.5 * (ub + lb)'
-        inputsHF_orig = 0.5 * inputsHF .* (ub - lb)' .+ 0.5 * (ub + lb)'
+        LF_data, HF_data = nothing, nothing
+        inputsLF_orig, inputsHF_orig = nothing, nothing
+        if batchID == 0
+            inputsLF_orig = 0.5 * inputsLF .* (ub - lb)' .+ 0.5 * (ub + lb)'
+            inputsHF_orig = 0.5 * inputsHF .* (ub - lb)' .+ 0.5 * (ub + lb)'
+            # input data is split into two parts: first half is GP points, second half is points through random acquisition. (The first k points in each are identical). We run the surrogate building loop twice, once for each half.
 
+            LF_data = generateLF(x, inputsLF_orig)
+            HF_data = generateHF(x, inputsHF_orig)
+        else
+            LF_data = generateLF(x, inputsLF)
+            HF_data = generateHF(x, inputsHF)
+        end
 
-        # input data is split into two parts: first half is GP points, second half is points through random acquisition. (The first k points in each are identical). We run the surrogate building loop twice, once for each half.
-
-
-
-        LF_data = generateLF(x, inputsLF_orig, nPilotLF, batch_num)
-        HF_data = generateHF(x, inputsHF_orig, nPilotHF, batch_num)
-
-        Y_Delta = HF_data - LF_data[:, inputsHFSubsetIdx]
+        Y_Delta = [HF_data[:, 1:nHF] - LF_data[:, inputsHFSubsetIdx[1:nHF]] HF_data[:, (nHF + 1):end] - LF_data[:, inputsHFSubsetIdx[(nHF + 1):end]]]
 
         # Generate k-fold indices (for half the dataset, repeat)
 
         k_folds_batch = k_folds(inputsHFSubsetIdx[1:nHF], args_dict["NFOLDS"]; rng_gen=MersenneTwister(rd_seed))
 
-        case_objects = Dict()
+        case_objects = []
+        cv_gp, oracle_gp, kle_gp = nothing, nothing, nothing
+        cv_ra, oracle_ra, kle_ra = nothing, nothing, nothing
         for case in kle_cases
             println("Rebuilding surrogate for case $case")
             if case == "gp"
-
+                cv_gp, oracle_gp, kle_gp = evaluateKLE(inputsLF[1:nLF, :], LF_data[:, 1:nLF], inputsHFSubsetIdx[1:nHF], inputsHF[1:nHF, :], HF_data[:, 1:nHF], Y_Delta[:, 1:nHF], x; 
+                useAbsErr=0, 
+                all_folds=k_folds_batch, 
+                grid_a_scaled=a_grid_scaled, 
+                grid_b_scaled=b_grid_scaled)
             elseif case == "ra"
-
+                cv_ra, oracle_ra, kle_ra = evaluateKLE(inputsLF[(nLF + 1):end, :], LF_data[:, (nLF + 1):end], inputsHFSubsetIdx[(nHF + 1):end], inputsHF[(nHF + 1):end, :], HF_data[:, (nHF + 1):end], Y_Delta[:, (nHF + 1):end], x; 
+                useAbsErr=0,
+                all_folds=k_folds_batch, 
+                grid_a_scaled=a_grid_scaled, 
+                grid_b_scaled=b_grid_scaled)
             end
-
         end
 
+        push!(case_objects, Dict("gp"=>(cv_gp, oracle_gp, kle_gp)))
+        push!(case_objects, Dict("ra"=>(cv_ra, oracle_ra, kle_ra)))
+
+        # Save the case objects to file
+        JLD.save(joinpath(args_dict["data_dir"], @sprintf("rep_%03d", repID), @sprintf("case_objects_batch_%03d.jld", batchID)), "case_objects", case_objects)
 
 
-        @pyinclude "./botorch_optim_point_selection_1d_final.py"
+        if batchID == 0
+            sys.argv = ["./1d_toy/botorch_optim_point_selection_1d_final.py",
+                rd_seed,
+                lb[1],
+                lb[2],
+                ub[1],
+                ub[2],
+                args_dict["plot_dir"],
+                args_dict["data_dir"],
+                args_dict["input_dir"],
+                repID,
+                batch_num,
+                log.(cv_gp[2]),
+                inputsHF_orig,
+                inputsLF_orig,
+                inputsHFSubsetIdx
+                ]
+        else
+            sys.argv = ["./1d_toy/botorch_optim_point_selection_1d_final.py",
+            rd_seed,
+            lb[1],
+            lb[2],
+            ub[1],
+            ub[2],
+            args_dict["plot_dir"],
+            args_dict["data_dir"],
+            args_dict["input_dir"],
+            repID,
+            batch_num,
+            log.(cv_gp[2]),
+            inputsHF,
+            inputsLF,
+            inputsHFSubsetIdx
+            ]
+        end
+        
 
-        acq_point = py"acq_point"
+            # torch_seed = sys.argv[1]
+            # lb = [sys.argv[2], sys.argv[3]]
+            # ub = [sys.argv[4], sys.argv[5]]
+            # plot_dir = sys.argv[6]
+            # data_dir = sys.argv[7]
+            # input_dir = sys.argv[8]
+            # rep_num = sys.argv[9]
+            # batch_num = sys.argv[10]
+            # cv_errors = sys.argv[11]
+            # hf_inputs = sys.argv[12] 
+            # lf_inputs = sys.argv[13]
+            # lf_subset = sys.argv[14]
 
-        println("Next training point for KLE:  $acq_point")
+        @pyinclude "./1d_toy/botorch_optim_point_selection_1d_final.py"
+
+        gp = py"gp"
+        utilities = py"acq"
+
+        open(joinpath(args_dict["data_dir"], @sprintf("rep_%03d", repID), @sprintf("gp_batch_%02d.pkl", batchID)), "w") do f
+            pkl.dump(Dict("gp"=>gp,
+                        "utilities"=>utilities), f)
+        end
 
     end
     println("Finished replication $repID")
